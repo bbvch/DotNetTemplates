6487c1e59e700a98db8c33c6b9823389303b7ac8
Raphael Schweizer <raphael.schweizer@bbv.ch>
2025-01-15 09:51:07 +0100

    .NET 9.0.1, remove FluentAssertions

--- console/src/ProjectNameToReplace.Tests/ProjectNameToReplaceVersionTests.cs
+++ console/src/ProjectNameToReplace.Tests/ProjectNameToReplaceVersionTests.cs
@@ -3,7 +3,6 @@ using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using FluentAssertions;
 using Xunit;
 
 namespace SolutionName.ProjectNameToReplace
@@ -16,19 +15,18 @@ namespace SolutionName.ProjectNameToReplace
         public void IsVersioned()
         {
             var version = this.assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-            version!.InformationalVersion.Should().MatchRegex(
-                Version(),
-                "the version should be suffixed by the git hash with dirty flag");
+            Assert.Matches(Version(), version?.InformationalVersion);
         }
 
         [Fact]
         public void IsTimestamped()
         {
             var metadata = this.assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
-            metadata.Single(m => m.Key == "BuildDate").Value.Should().Match(buildDate => Timestamp(buildDate));
+            var buildDate = metadata.Single(m => m.Key == "BuildDate").Value;
+            Assert.True(Timestamp(buildDate));
         }
 
-        private static bool Timestamp(string value) => DateTime.TryParse(value, CultureInfo.InvariantCulture, out _);
+        private static bool Timestamp(string? value) => DateTime.TryParse(value, CultureInfo.InvariantCulture, out _);
 
         [GeneratedRegex(@"\d.\d.\d\+[a-z0-9]{7,}\*?")]
         private static partial Regex Version();
